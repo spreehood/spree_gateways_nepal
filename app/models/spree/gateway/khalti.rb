24332c5340282f41099209124e29c3a09945f888
Prakash Poudel <prakash@truemark.com.np>
2020-09-03 22:09:08 +0545

    Js for submit

--- app/models/spree/gateway/khalti.rb
+++ app/models/spree/gateway/khalti.rb
@@ -1,4 +1,10 @@
 class Spree::Gateway::Khalti < Spree::Gateway
+  attr_accessor :server, :test_mode
+
+
+  preference :server,:string, default: 'test'
+  preference :test_mode, :boolean, default: true
+
   def provider_class
     Spree::Gateway::Khalti
   end
@@ -11,6 +17,7 @@ class Spree::Gateway::Khalti < Spree::Gateway
   end
 
   def purchase(amount, transaction_details, options = {})
+    binding.pry
     ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
   end
 end
