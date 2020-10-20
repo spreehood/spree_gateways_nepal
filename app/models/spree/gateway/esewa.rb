6c82ddbec59c5a8feb71784a98380322a3b56226
roringbbk <bbk.programmerz@gmail.com>
2020-10-21 01:21:56 +1100

    Add payment service for epay

--- app/models/spree/gateway/esewa.rb
+++ app/models/spree/gateway/esewa.rb
@@ -1,14 +1,27 @@
 module Spree
   class Gateway::Esewa < Gateway
-    preference :mode, :string
     preference :sandbox_url, :string
     preference :production_url, :string
-    preference :scd, :string, default: 'epay_payment'
-    preference :username, :string
-    preference :password, :string
+    preference :merchant_code, :string, default: 'epay_payment'
+    preference :successful_callback_url, :string
+    preference :failure_callback_url, :string
+    preference :fraud_check_url, :string
+    preference :server, :select, default: -> { { values: [:sandbox, :production] } }
 
     def provider_class
-      ActiveMerchant::Billing::EsewaGateway
+      Esewa
+    end
+
+    def auto_capture?
+      true
+    end
+
+    def method_type
+      'esewa'
+    end
+
+    def purchase
+      # TODO: Trigger esewa payment service
     end
   end
 end
